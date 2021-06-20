83dcc421f80c17cbcb703ae9eb7d4b2b6b2fc346
ghiliweld <ghiliaweld@gmail.com>
2021-06-20 13:21:13 -0400

    plz

--- api/index.js
+++ api/index.js
@@ -5,13 +5,14 @@ const app = express()
 const port = 3000
 
 app.use(cors())
-app.use(bodyParser.json({ limit: '150mb' }))
+// app.use(bodyParser.json({ limit: '150mb' }))
 app.use(express.json());
 
-const board = {};
+const board = new Map();
+
 for (let i = 1; i < 11; i++) {
   for (let j = 1; j < 11; j++) {
-    board[`${i}_${j}`] = '.';
+    board.set(`${i}_${j}`, '.');
   }
 }
 
@@ -25,32 +26,14 @@ app.get('/api/board', (req, res) => {
 
 app.get('/api/:coords', (req, res) => {
     const { coords } = req.params;
-    res.send(board[coords]);
+    res.json({ char: board.get(coords) });
 })
 
 app.post('/api/:coords', (req, res) => {
     const { coords } = req.params;
     const { char } = req.body;
-    board[coords] = char;
-    res.send('success!');
+    board.set(coords, char);
+    res.json({ msg: 'success!' });
 })
 
-// const app = require('express')()
-// // const { v4 } = require('uuid')
-
-// app.get('/api', (req, res) => {
-//   // const path = `/api/item/${v4()}`
-//   res.setHeader('Content-Type', 'text/html')
-//   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
-//   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`)
-// })
-
-// app.get('/api/item/:slug', (req, res) => {
-//   const { slug } = req.params
-//   res.end(`Item: ${slug}`)
-// })
-
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`)
-// })
 module.exports = app;
