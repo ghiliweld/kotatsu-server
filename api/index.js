88236116d73be2559a3bda43a9ce6c6ab0715dfb
ganer <ganeralkidz@gmail.com>
2021-07-06 20:48:18 -0400

    put request, cleaned code

--- api/index.js
+++ api/index.js
@@ -1,47 +1,33 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser');
-const app = express()
-const port = 3000
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
-app.use(cors())
-// app.use(bodyParser.json({ limit: '150mb' }))
+app.use(cors());
 app.use(express.json());
 
 const board = {};
 for (let i = 1; i < 11; i++) {
   for (let j = 1; j < 11; j++) {
-    board[`${i}_${j}`] = '.';
+    board[`${i}_${j}`] = ".";
   }
 }
 
-app.get('/api', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/api/board", (req, res) => {
+  res.json(board);
+});
 
-app.get('/api/board', (req, res) => {
-  res.json(board)
-})
-
-app.post('/api/board', (req,res) => {
+app.post("/api/board", (req, res) => {
   for (let i = 1; i < 11; i++) {
     for (let j = 1; j < 11; j++) {
-      board[`${i}_${j}`] = '.';
+      board[`${i}_${j}`] = ".";
     }
   }
-  res.json({msg: 'success!'})
-})
+  res.json({ msg: "success!" });
+});
 
-app.get('/api/:coords', (req, res) => {
-    const { coords } = req.params;
-    res.json({ char: board[coords] });
-})
-
-app.post('/api/:coords', (req, res) => {
-    const { coords } = req.params;
-    const { char } = req.body;
-    board[coords] = char;
-    res.json({ msg: 'success!' });
-})
+app.put("/api/board", (req, res) => {
+  board[coord] = req.body.char;
+  res.json({ msg: "success!" });
+});
 
 module.exports = app;
