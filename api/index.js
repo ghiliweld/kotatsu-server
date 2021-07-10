5a985402e2d6d2ebbeb3ca11e3d1781af76a07af
halbouz <ganeralkidz@gmail.com>
2021-07-10 19:07:18 -0400

    testing cors

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ let versionNum = 0;
 var subscriptions = {}
 var subscription_hash = (req) => JSON.stringify([req.headers.peer, req.url])
 
-app.use(cors())
+app.options('*', cors())
 app.use(express.json());
 //app.use(free_the_cors());
 app.use(braidify);    // Add braid stuff to req and res
